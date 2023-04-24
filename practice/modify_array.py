7beb39e9db694c01b0e862f6efdeda35f0ece1d1
ImagawaKosuke <kosuke8de@gmail.com>
2023-04-24 11:23:31 +0900

    GPT
    ChatGPTの文字列の処理を完了しました

---
+++ practice/modify_array.py
@@ -0,0 +1,23 @@
+import re
+
+answer = ('''1. [ポケモン最高！] ("Pokemon saikou!")
+2. [俺もジムリーダー目指す！] ("Ore mo Jimu Ridaa mezasu!")
+3. [ピカチュウ可愛すぎる！] ("Pikachuu kawaisugiru!")
+4. [対戦で負けた…反省する。] ("Taisen de maketa... hansei suru.")
+5. [ルカリオ大好き！] ("Lucario daisuki!")
+6. [新ポケモンのデザインはどうだろう？] ("Shin Pokemon no dezain wa dou darou?")
+7. [サトシがリーグ優勝するといいな。] ("Satoshi ga Rigu yuushou suru to ii na.")
+8. [豆知識：ニドキングは世界最強のポケモンの一つだ。] ("Mame chishiki: Nidokingu wa sekai saikyou no Pokemon no hitotsu da.")
+9. [あのポケモンはどこで手に入るの？] ("Ano Pokemon wa doko de te ni hairu no?")
+10. [シンジ大好き！] ("Shinji daisuki!")''')
+answer = answer.splitlines()
+answers = []
+for i in range(len(answer)):
+    new_answers = re.sub("\[.+?\]", "", answer[i])
+    new_answers = re.sub("\"|\"", "", new_answers)
+    answers.append(new_answers)
+    
+for i in range(len(answers)):
+    if answers[i].find(str(i+1)+'.  ') != -1:
+        answers[i] = answers[i].replace(str(i+1)+'.  ','')
+print(answers)
